7cc5f22b34ec9214d075e820b6a96e081b79b9ab
Michael Sumerano <5069447+sumo300@users.noreply.github.com>
2013-06-21 01:21:37 -0400

    Added Format-Path posh function that shortens a path ala Vim

---
+++ SeeSharpShell.Tests/Format_Path_Tests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Management.Automation;
+using NUnit.Framework;
+
+namespace SeeSharpShell.Tests {
+    [TestFixture]
+    public class Format_Path_Tests {
+        [Test]
+        public void Ctor_ShouldCreateCmdLet()
+        {
+            var formatPath = new Format_Path();
+            var cmdlet = formatPath as Cmdlet;
+
+            Assert.IsTrue(cmdlet != null);
+        }
+
+        [Test]
+        public void HomePath_ShouldReturnShortenedPathWithTilde()
+        {
+            string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string testPath = Path.Combine(home, @"Hello\World");
+            const string pathToValidate = @"~\H\World";
+
+            var cmd = new Format_Path {Path = testPath};
+            IEnumerator result = cmd.Invoke().GetEnumerator();
+
+            Assert.IsTrue(result.MoveNext());
+            Assert.IsTrue(result.Current is string);
+
+            Assert.That(result.Current.ToString(), Is.EqualTo(pathToValidate));
+        }
+
+        [Test]
+        public void LocalPath_ShouldReturnShortenedPath()
+        {
+            const string testPath = @"C:\Hello\World";
+            const string pathToValidate = @"C:\H\World";
+
+            var cmd = new Format_Path {Path = testPath};
+            IEnumerator result = cmd.Invoke().GetEnumerator();
+
+            Assert.IsTrue(result.MoveNext());
+            Assert.IsTrue(result.Current is string);
+
+            Assert.That(result.Current.ToString(), Is.EqualTo(pathToValidate));
+        }
+
+        [Test]
+        public void UNCPath_ShouldReturnShortenedPath()
+        {
+            const string testPath = @"\\SERVER\Hello\World";
+            const string pathToValidate = @"\\S\H\World";
+
+            var formatPath = new Format_Path {Path = testPath};
+            IEnumerator result = formatPath.Invoke().GetEnumerator();
+
+            Assert.IsTrue(result.MoveNext());
+            Assert.IsTrue(result.Current is string);
+
+            Assert.That(result.Current.ToString(), Is.EqualTo(pathToValidate));
+        }
+    }
+}
---
+++ SeeSharpShell/Format_Path.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Management.Automation;
+using System.Text.RegularExpressions;
+
+namespace SeeSharpShell {
+    [Cmdlet(VerbsCommon.Format, "Path")]
+    public class Format_Path : Cmdlet {
+        [Parameter(Position = 0, Mandatory = true)]
+        public string Path { get; set; }
+
+        protected override void EndProcessing()
+        {
+            if (string.IsNullOrWhiteSpace(Path)) {
+                WriteWarning("Path must be supplied.");
+            }
+
+            string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string shortened = Path.Replace(home, "~");
+
+            // Remove prefixes for UNC paths
+            shortened = new Regex("^[^:]+::").Replace(shortened, string.Empty);
+
+            // Make path shorter like tabs in Vim, plus handle paths starting
+            // with \\ and . correctly
+            WriteObject(new Regex(@"\\(\.?)([^\\])[^\\]*(?=\\)").Replace(shortened, @"\$1$2"));
+        }
+    }
+}
